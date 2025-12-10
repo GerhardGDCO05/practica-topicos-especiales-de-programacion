25e8b3aae485c262afe6fbaec3e8a846260ba8cc
LAB. INGENIERIA L236 <LAB_L236@LABSUCAB.UCAB.EDU.VE>
2025-12-10 10:18:24 -0400

    mongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express()
 dotenv.config
+const connectDB=()=>{
+    const{
+        MONGO_USERNAME,
+        MONGO_PASSWORD,
+        MONGO_HOSTNAME,
+        MONGO_PORT,
+        MONGO_DB
+    }=process.env
+
+    const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+    mongoose.connect(url).then(function () {
+        console.log("[+] Conexion con bd exitosa!");
+    })
+    .catch((err) => console.log(`[-] ${err}`));
+};
+
 
 const port = 3005
 app.use(cors({ origin: '*' })) // cors
@@ -11,6 +28,7 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, function () {
+    connectDB()
     console.log(`Api corriendo en http://localhost:${port}!`)
 })
 
